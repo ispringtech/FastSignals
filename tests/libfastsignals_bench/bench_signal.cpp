b469343b72f05337c23a023f6874fc8d8ca714eb
Sergey Shambir <sergey.shambir@ispringsolutions.com>
2018-07-28 10:10:03 +0300

    Updated benchmark to measure signal emission with different connections count Applied formatting

--- tests/libfastsignals_bench/bench_signal.cpp
+++ tests/libfastsignals_bench/bench_signal.cpp
@@ -1,25 +1,33 @@
 #include "stdafx.h"
-#include <benchmark/benchmark.h>
 #include "libfastsignals/signal.h"
+#include <benchmark/benchmark.h>
 #include <boost/signals2.hpp>
+
+namespace
+{
+unsigned kBenchMaxConnections = 8;
+unsigned kBenchEmitCount = 500;
+} // namespace
 
 void emit_fastsignals(benchmark::State& state)
 {
 	using namespace is::signals;
 	for (auto _ : state)
 	{
-		// Do not modify
 		volatile int savedValue = 0;
-		signal<void(int)> valueChanged;
-		for (unsigned i = 0; i < 10; ++i)
+		for (unsigned connCount = 0; connCount < kBenchMaxConnections; ++connCount)
 		{
-			valueChanged.connect([&savedValue](int value) {
-				savedValue = value;
-			});
-		}
-		for (unsigned i = 0; i < 1000; ++i)
-		{
-			valueChanged(i);
+			signal<void(int)> valueChanged;
+			for (unsigned i = 0; i < connCount; ++i)
+			{
+				valueChanged.connect([&savedValue](int value) {
+					savedValue = value;
+				});
+			}
+			for (unsigned i = 0; i < kBenchEmitCount; ++i)
+			{
+				valueChanged(i);
+			}
 		}
 	}
 }
@@ -29,18 +37,20 @@ void emit_boost(benchmark::State& state)
 	using namespace boost::signals2;
 	for (auto _ : state)
 	{
-		// Do not modify
 		volatile int savedValue = 0;
-		signal<void(int)> valueChanged;
-		for (unsigned i = 0; i < 10; ++i)
+		for (unsigned connCount = 0; connCount < kBenchMaxConnections; ++connCount)
 		{
-			valueChanged.connect([&savedValue](int value) {
-				savedValue = value;
-			});
-		}
-		for (unsigned i = 0; i < 1000; ++i)
-		{
-			valueChanged(i);
+			signal<void(int)> valueChanged;
+			for (unsigned i = 0; i < connCount; ++i)
+			{
+				valueChanged.connect([&savedValue](int value) {
+					savedValue = value;
+				});
+			}
+			for (unsigned i = 0; i < kBenchEmitCount; ++i)
+			{
+				valueChanged(i);
+			}
 		}
 	}
 }

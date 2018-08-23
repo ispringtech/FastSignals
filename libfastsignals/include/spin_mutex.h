1f90e88964e2e8c15fa849527242703c5c20a776
Sergey Shambir <sergey.shambir@ispringsolutions.com>
2018-08-23 07:48:34 +0300

    spin_mutex now has deleted copy/move constructors/operators

--- libfastsignals/include/spin_mutex.h
+++ libfastsignals/include/spin_mutex.h
@@ -7,6 +7,12 @@ namespace is::signals
 class spin_mutex
 {
 public:
+	spin_mutex() = default;
+	spin_mutex(const spin_mutex&) = delete;
+	spin_mutex& operator=(const spin_mutex&) = delete;
+	spin_mutex(spin_mutex&&) = delete;
+	spin_mutex& operator=(spin_mutex&&) = delete;
+
 	inline bool try_lock()
 	{
 		return !m_busy.test_and_set(std::memory_order_acquire);

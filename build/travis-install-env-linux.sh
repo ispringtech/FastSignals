4a7a29f52e5329d596592b99fe288e54cfcbf799
Sergey Shambir <sergey.shambir@ispringsolutions.com>
2018-08-27 12:00:18 +0300

    Reverted Travis script changes

--- build/travis-install-env-linux.sh
+++ build/travis-install-env-linux.sh
@@ -1,5 +1,8 @@
 
 # install GCC
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq
+sudo apt-get install -qq g++-8
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 90
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 90
 sudo update-alternatives --install /usr/bin/cpp cpp /usr/bin/cpp-8 90

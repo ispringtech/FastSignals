c137a0b4a19ada44a07427835fec0c88721f3d28
Sergey Shambir <sergey.shambir@ispringsolutions.com>
2018-09-10 13:19:39 +0300

    Cleanup MSVC build settings Cleanup msvc_autolink.h (removed second __clang__ block) Enabled code analysis on build

--- libfastsignals/include/msvc_autolink.h
+++ libfastsignals/include/msvc_autolink.h
@@ -14,16 +14,6 @@
 #		endif
 #	elif _MSC_VER <= 1900
 #		error this library needs Visual Studio 2017 and higher
-#	elif defined(__clang__)
-#		if defined(_DEBUG) && defined(_WIN64)
-#			pragma comment(lib, "libfastsignalsd-llvm-x64.lib")
-#		elif defined(_DEBUG)
-#			pragma comment(lib, "libfastsignalsd-llvm-x32.lib")
-#		elif defined(_WIN64)
-#			pragma comment(lib, "libfastsignals-llvm-x64.lib")
-#		else
-#			pragma comment(lib, "libfastsignals-llvm-x32.lib")
-#		endif
 #	elif _MSC_VER < 2000
 #		if defined(_DEBUG) && defined(_WIN64)
 #			pragma comment(lib, "libfastsignalsd-v141-x64.lib")
